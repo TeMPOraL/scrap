879979d8ba93be15d2034819e33dea14eb56a024
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2016-04-03 00:25:47 +0200

    Initial parsing of oglaszamy24 results.
    - NOTE will likely break if anything changes in the site structure
    - added: .gitignore
    - added: extractor of text value of DOM node stolen from Webgunk project

--- oglaszamy24.lisp
+++ oglaszamy24.lisp
@@ -12,10 +12,18 @@
   (chtml:parse (fetch-oglaszamy24-raw-results search)
                (cxml-dom:make-dom-builder)))
 
+(defun process-ad (ad)
+  (let ((ad-title-node (css:query1 "a.o_title" ad))
+        (ad-description-node (css:query1 "div.o_single_box > div:first-child > div:nth-child(4) > div:nth-child(4)" ad)))
+
+    (list (node-text ad-title-node)
+          (node-text ad-description-node))))
+
+(defun process-ads (ad-node-list)
+  (mapcar #'process-ad ad-node-list))
+
 (defun list-ads (dom)
-  (css:query "")
-  )
+  (process-ads (css:query "div.o_single_box" dom)))
 
 (defun fetch-oglaszamy24-results (search)
-  
-)
+  (list-ads (fetch-oglaszamy24-dom search)))
